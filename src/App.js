f2ddc296ac905979dd3bc0f43e56236e368f9dfb
rakinplaban <rakinshahriar54@gmail.com>
2024-09-25 17:10:26 +0600

    added css to square boards.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Square({value}) {
+  function handleClick() {
+    console.log("clicked");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="square" onClick={handleClick}>{value}</button>
+  );
+      
+}
+
+function Board() {
+  return (
+    <>
+    <div className="board-row">
+      <Square value="1"/>
+      <Square value="2"/>
+      <Square value="3"/>
     </div>
+    <div className="board-row">
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
+    </div>
+    <div className="board-row">
+      <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>
+    </div>
+  </>
   );
 }
 
-export default App;
+export default Board;
